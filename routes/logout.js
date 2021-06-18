0665e33135e98c0ec88af8b018d690b668655c61
aseer <ajjaatar.tech@gmail.com>
2021-06-19 09:22:25 +1000

    login works

--- routes/logout.js
+++ routes/logout.js
@@ -2,36 +2,23 @@ const express = require('express')
 const router = express.Router()
 
 
-// router.get('/', (req, res) => {
-//   if (req.session) {
-//     res.clearCookie('cookie', { path: '/' })
-//     req.session.destroy(err => {
-//       if (err) {
-//         console.log('error')
-//         res.status(400).send('Unable to log out')
-//       } else {
-//         console.log('2nd last else')
-//         res.clearCookie('cookie', { path: '/' })
-//         res.render('pages/login')
-//       }
-//     });
-//   } else {
-//     console.log('last else part')
-//     res.render('pages/login')
-//   }
-// })
-
 router.get('/', (req, res) => {
-  console.log('first scope')
-  req.session.destroy( err => {
-    console.log('second scope')
-    if (err) {
-      console.log(err)
-      res.send('error')
-    } else {
-      res.render('pages/login', {logout: "logout successful"})
-    }
-  })
+  if (req.session) {
+    res.clearCookie('cookie')
+    req.session.destroy(err => {
+      if (err) {
+        console.log('error')
+        res.status(400).send('Unable to log out')
+      } else {
+        console.log('2nd last else')
+        res.clearCookie('cookie')
+        res.redirect('/login')
+      }
+    });
+  } else {
+    console.log('last else part')
+    res.redirect('/login')
+  }
 })
 
 module.exports = router
