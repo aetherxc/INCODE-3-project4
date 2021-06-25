d660c09373d34ba245c92e291ac111ed9e99e91e
Anthony Buccat <a.buccat@ilcloud.com>
2021-06-25 16:46:42 +1000

    manage schdule added

--- routes/new_schedule.js
+++ routes/new_schedule.js
@@ -9,10 +9,28 @@ const session = require('express-session')
 
 // get req 
 router.get('/', redirectToLogin, (req, res) => {
-  res.render('pages/new_schedule', {
-    message: req.query.message,
-    id:req.session.userId
-  })
+  db.any('SELECT \
+          users.firstname, users.surname, users.email, \
+          schedules.day, schedules.start_time, schedules.end_time \
+        FROM \
+          users \
+        INNER JOIN \
+          schedules \
+        ON \
+          users.id = schedules.id_user \
+        WHERE \
+          users.id = $1;', [req.params.id? req.params.id : req.session.userId])
+    .then((schedule) => {
+      console.log(schedule)
+      res.render('pages/new_schedule', {
+        message: req.query.message,
+        employeeSchedule: schedule,
+        id:req.session.userId
+      })
+    })
+    .catch((err) => {
+      res.send(err)
+    })
 })
 
 // post req
@@ -23,7 +41,9 @@ router.post('/', redirectToLogin, (req, res) => {
     start_time: req.body.startTime,
     end_time: req.body.endTime
   }
-  db.none('INSERT INTO schedules(id_user, day, start_time, end_time) VALUES ($1, $2, $3, $4);', [newSchedule.id_user, newSchedule.day, newSchedule.start_time, newSchedule.end_time])
+  db.none('INSERT INTO \
+            schedules(id_user, day, start_time, end_time) \
+          VALUES ($1, $2, $3, $4);', [newSchedule.id_user, newSchedule.day, newSchedule.start_time, newSchedule.end_time])
     .then(() => {
       res.redirect('/new_schedule?message=new%20schedule%20created.')
     })
@@ -32,8 +52,8 @@ router.post('/', redirectToLogin, (req, res) => {
       const message = err.message.replace(/ /g, '%20')
       res.redirect(`/new_schedule?message=${message}`)
     })
+  
 })
-
 
 
 module.exports = router
